35b44110176b1ee77a87a17e9493f653540a948a
0l3h <kubik7950@gmail.com>
2021-11-07 11:10:29 +0200

    Removed src, changed configs

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const http = require('http')
+require('./models/mongoModels')
+const controller = require('./socketInit')
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => 
+    console.log(`\nExample app listening on port ${PORT}!\n`)
+)
+
+controller.createConnection(server)
